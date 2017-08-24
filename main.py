f4766f761d177ade279d837b505d822e5b63a07d
Shiwen Zhang <shi.safari@gmail.com>
2017-08-24 11:39:58 +0800

    Define pages
    This update do simple work, add pages. That is it, nothing to talk about.
    
    Here is list need to fixed:
    
    * change label size and format
    * change line edit size
    * implement back event
    
    Signed-off-by: Shiwen Zhang <shi.safari@gmail.com>

--- main.py
+++ main.py
@@ -16,55 +16,121 @@ def main():
 	app = QtGui.QApplication(sys.argv);
 	wiz = QtGui.QWizard();
 
-	namepage = createNamepage(wiz);
-	secpage = createWelcomepage(wiz);
-	happypage = createHappypage(wiz);
-	misspage = createMisspage(wiz);
+	namePage = createNamePage(wiz);
+	secPage = createWelcomePage(wiz);
+	happyPage = createHappyPage(wiz);
+	missPage = createMissPage(wiz);
+	candyPage = createCandyPage(wiz);
+	eatcandyPage = createEatCandyPage(wiz);
+	sweetPage = createSweetPage(wiz);
+	sweetyouPage = createSweetYouPage(wiz);
+	sweetPage = createSweetPage(wiz);
+	meatPage = createMeatPage(wiz);
+	eatmeatPage = createEatMeatPage(wiz);
 
-	wiz.addPage(namepage);
-	wiz.addPage(secpage);
-	wiz.addPage(happypage);
-	wiz.addPage(misspage);
+	wiz.addPage(namePage);
+	wiz.addPage(secPage);
+	wiz.addPage(happyPage);
+	wiz.addPage(missPage);
+	wiz.addPage(candyPage);
+	wiz.addPage(eatcandyPage);
+	wiz.addPage(sweetPage);
+	wiz.addPage(sweetyouPage);
+	wiz.addPage(meatPage);
+	wiz.addPage(eatmeatPage);
 	Wizard.setwindow(wiz);
 	wiz.show();
 
 	sys.exit(app.exec_());
 
 
-def createNamepage(wiz):
-	page,layout = Wizard.createPage()
+def createNamePage(wiz):
+	Page,Layout = Wizard.createPage()
 
-	Wizard.setPageLabel(unicode("你叫什么名字呀！:)",'utf-8'),page,layout);
-	edit = Wizard.setInput(page,layout);
-	Wizard.setButton(page,"",unicode("我写好了","utf-8"),"");
+	Wizard.setPageLabel(unicode("你叫什么名字呀！:)",'utf-8'),Page,Layout);
+	edit = Wizard.setInput(Page,Layout);
+	Wizard.setButton(Page,"",unicode("我写好了","utf-8"),"");
 
-	return page;
+	return Page;
 
-def createWelcomepage(wiz):
-	page,layout = Wizard.createPage()
+def createWelcomePage(wiz):
+	Page,Layout = Wizard.createPage()
 	
-	Wizard.setPageLabel(unicode("太好了，我就是找你呀！ :)",'utf-8'),page,layout);
-	Wizard.setPageLabel(unicode("今天开不开心呀? :)",'utf-8'),page,layout);
+	Wizard.setPageLabel(unicode("太好了，我就是找你呀！ :)",'utf-8'),Page,Layout);
+	Wizard.setPageLabel(unicode("今天开不开心呀? :)",'utf-8'),Page,Layout);
 
-	Wizard.setButton(page,unicode("不开心","utf-8"),unicode("当然开心啦","utf-8"),"")
+	Wizard.setButton(Page,unicode("不开心","utf-8"),unicode("当然开心啦","utf-8"),"")
 
-	return page;
+	return Page;
 
-def createHappypage(wiz):
-	page,layout = Wizard.createPage()
+def createHappyPage(wiz):
+	Page,Layout = Wizard.createPage()
 	
-	Wizard.setPageLabel(unicode("今天在干嘛呀 :)",'utf-8'),page,layout);
-	Wizard.setButton(page,unicode("当然是工作啦","utf-8"),unicode("在想你呀","utf-8"),
-			unicode("七夕快乐呀，大臭臭 :)","utf-8"))
+	Wizard.setPageLabel(unicode("今天在干嘛呀 :)",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("当然是工作啦","utf-8"),unicode("在想你呀","utf-8"),
+			"")
 
-	return page;
+	return Page;
 
-def createMisspage(wiz):
-	page,layout = Wizard.createPage()
+def createMissPage(wiz):
+	Page,Layout = Wizard.createPage()
 	
-	Wizard.setPageLabel(unicode("在想我呀，我也想你呢 :)",'utf-8'),page,layout);
+	Wizard.setPageLabel(unicode("在想我呀，我也想你呢 :)",'utf-8'),Page,Layout);
+	Wizard.setPageLabel(unicode("对呀，超级超级想你呢！ :)",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("哼，我才不信呢","utf-8"),unicode("我相信啦","utf-8"),
+			unicode("七夕快乐呀，大臭臭 :)","utf-8"));
 
-	return page;
+	return Page;
 
+def createCandyPage(wiz):
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("哈哈，我们过得第一个七夕节呢，送给宝宝的零食礼包喜欢吗？",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("不喜欢","utf-8"),unicode("喜欢呀","utf-8"),"");
+
+	return Page;
+
+def createEatCandyPage(wiz):
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("好了，吃一块糖好不好呀？",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("不想吃，心情不好","utf-8"),unicode("正在吃呢","utf-8"),"");
+
+	return Page;
+
+def createSweetPage(wiz):
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("甜不甜呀？",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("不甜","utf-8"),unicode("甜呀","utf-8"),"");
+
+	return Page;
+
+def createSweetYouPage(wiz):
+
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("那是因为老公很甜呀，甜甜蜜蜜的呢! :)",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("我还要听一遍","utf-8"),unicode("我要看下一页","utf-8"),
+			unicode("七夕快乐呀，大臭臭 :)","utf-8"));
+
+	return Page;
+
+def createMeatPage(wiz):
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("饿不饿呀，要不要吃一块肉肉？",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("哼，不吃","utf-8"),unicode("要吃要吃","utf-8"),"");
+
+	return Page;
+
+def createEatMeatPage(wiz):
+	Page,Layout = Wizard.createPage()
+	
+	Wizard.setPageLabel(unicode("多吃一点，老公这么好看，吃什么都不会胖的！:)",'utf-8'),Page,Layout);
+	Wizard.setButton(Page,unicode("再吃一块肉肉","utf-8"),unicode("我要吃别的","utf-8"),
+			unicode("七夕快乐呀，大臭臭 :)","utf-8"));
+
+	return Page;
 
 main();
